7e83cbad857db006ebfdbf662151bf8c6f5cb206
fa473 <home@homes-MacBook-Pro.local>
2017-04-06 22:21:33 -0700

    removed comments-data.js

--- data/comments-data.js
+++
@@ -1,6 +0,0 @@
- var SERVER_URL = 'http://localhost:3002'
- var commentsArray = '';
- $.get(SERVER_URL + '/' + 'commentsArray', function(data){
-   commentsArray = data;
- });
- console.log(commentsArray);
